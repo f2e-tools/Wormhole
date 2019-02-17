225850888d9f91f78c04a47d3a51b6293fb929d2
AllenChinese <13402202685@163.com>
2019-02-17 22:08:21 +0800

    feat: common components file

---
+++ src/components/AppNotice.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-notice">app 告示</div>
+</template>
+<script>
+export default {
+  name: 'appNotice'
+}
+</script>
+<style lang="scss" scoped>
+.app-notice {
+  padding: 5vh 30vw;
+  width: 100%;
+  height: 45vh;
+  text-align: center;
+  background-color: #00a6ac;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as AppNotice } from './AppNotice.vue'

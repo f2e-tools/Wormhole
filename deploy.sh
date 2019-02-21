db2ca1f0cc66c318df50850eadbbf6a3a8f65991
AllenChinese <13402202685@163.com>
2019-02-21 12:51:41 +0800

    chore: setting shell script for deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+#生成当前时间
+time=`date '+%Y%m%d-%H%M%S'`
+
+#确保脚本抛出遇到的错误
+set -e
+
+#生成静态文件
+npm run build
+
+#进入生成的文件夹
+cd dist
+
+#如果是自定义域名 指定解析值
+# echo "" > CNAME
+
+git init
+git add -A
+git commit -m "deploy-$time"
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:allenchinese/Wormhole.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,9 @@
-module.export = {
+module.exports = {
   lintOnSave: false,
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.json', '.vue'],
     },
   },
-  baseUrl: '',
-  rules: [{
-    test: /\.scss$/,
-    use: [
-      "style-loader", // creates style nodes from JS strings
-      "css-loader", // translates CSS into CommonJS
-      "sass-loader" // compiles Sass to CSS, using Node Sass by default
-    ]
-  }]
+  publicPath: '/Wormhole'
 }
